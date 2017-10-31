b8e655361db7b1512751dd25d1950a2927e3ba03
Masashi_Yuasa <masashi_yuasa@jmas.co.jp>
2017-10-31 11:27:11 +0900

    add sensor setup script

---
+++ sensor_setup.sh
@@ -0,0 +1,6 @@
+sudo mv /home/aws/app/aws-send.py /home/aws/app/aws-send2.py
+sudo mv /home/aws/app/sensor.py /home/aws/app/sensor2.py
+
+sudo cp aws-send.py /home/aws/app/
+sudo cp sensor.py /home/aws/app/
+
