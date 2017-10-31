304e09b4b8aae772fa33ed762cdc982d2fecb2db
Masashi_Yuasa <masashi_yuasa@jmas.co.jp>
2017-10-31 16:35:30 +0900

    redis dummy data

---
+++ redis_push.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+#!/usr/bin/env python
+# coding: utf-8
+
+import redis
+import json
+import time
+import argparse
+import sys
+import paho.mqtt.client as mqtt
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+from datetime import datetime
+import socket
+
+opt = argparse.ArgumentParser(description='xxx')
+opt.add_argument('-i', '--id',       help='`product id', default='iot')
+opt.add_argument('-e', '--endpoint', help='`aws endpoint', default='')
+opt.add_argument('-t', '--tenantid', help='`tenant id',   default='test')
+#opt.add_argument('-u', '--usb',    help='USB Device',  default='/dev/ttyACM0')
+args = opt.parse_args()
+
+conn = redis.StrictRedis(host='localhost', port=6379)
+key  = 'sensor/send'
+
+count = 0
+while True:
+   topic = args.id + '/' + args.tenantid + '/sensor'
+   n = datetime.now()
+   st = (int(n.strftime('%s')) / 60) * 60
+   now = n.isoformat()
+   hostname = socket.gethostname()
+   did = "ttySYSTEM0"
+   msg_ori = {
+      "id": hostname + "/" + did,
+      "device": did,
+      "hostname": hostname,
+      "timeserial": st,
+      "timestamp": now,
+      "value": 1,
+   }
+   msg = json.dumps(msg_ori)
+   print(host_type + ':' + topic + ':' + msg + "\n")
+   conn.rpush('sensor/send', msg)
+   print("success push:", count)
+   count += 1
+   time.sleep(5)
+
