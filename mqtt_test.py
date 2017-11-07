f98d6789df7890e6970f64ecaa48881c93468162
Masashi_Yuasa <masashi_yuasa@jmas.co.jp>
2017-11-07 09:54:07 +0900

    add connnect

--- mqtt_test.py
+++ mqtt_test.py
@@ -10,5 +10,11 @@ conn = redis.StrictRedis(host="localhost", port=6379)
 msg = conn.lindex(key, 0)
 
 mq = mqtt.Client(protocol=mqtt.MQTTv311)
+mq.connect()
 mq_result, _ = mq.publish(topic, msg, 0)
 
+if mq_result is mqtt.MQTT_ERR_SUCCESS:
+    print("success")
+else:
+    print("failure")
+    print(mq_result)
